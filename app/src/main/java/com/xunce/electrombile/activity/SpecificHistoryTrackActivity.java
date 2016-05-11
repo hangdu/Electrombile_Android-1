659729b1b41d8769abd7f2277c1752d4a12c8057
hangdu <2580392278@qq.com>
2016-05-11 21:09:04 +0800

    把轨迹改成了红色 加粗

--- app/src/main/java/com/xunce/electrombile/activity/SpecificHistoryTrackActivity.java
+++ app/src/main/java/com/xunce/electrombile/activity/SpecificHistoryTrackActivity.java
@@ -321,8 +321,8 @@ public class SpecificHistoryTrackActivity extends Activity {
         //构建用户绘制多边形的Option对象
         OverlayOptions polylineOption = new PolylineOptions()
                 .points(points)
-                .width(10)
-                .color(0xAA00FF00);
+                .width(15)
+                .color(0xAAFF0000);
         //在地图上添加多边形Option，用于显示
        mBaiduMap.addOverlay(polylineOption);
     }
