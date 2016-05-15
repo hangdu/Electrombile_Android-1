d0524844394751a0c609ad6115bf81869225e944
hangdu <duhang1234@qq.com>
2016-05-15 22:32:21 +0800

    可以实现版本的自动更新了

--- app/src/main/java/com/xunce/electrombile/activity/SplashActivity.java
+++ app/src/main/java/com/xunce/electrombile/activity/SplashActivity.java
@@ -13,6 +13,7 @@ import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 
 import com.avos.avoscloud.AVUser;
 import com.avos.avoscloud.LogUtil;
@@ -20,11 +21,9 @@ import com.xunce.electrombile.Constants.ServiceConstants;
 import com.xunce.electrombile.R;
 import com.xunce.electrombile.activity.account.LoginActivity;
 import com.xunce.electrombile.activity.account.VerifiedActivity;
+import com.xunce.electrombile.utils.useful.JSONUtils;
 import com.xunce.electrombile.utils.useful.NetworkUtils;
 
-import org.json.JSONObject;
-
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
@@ -32,6 +31,9 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Random;
+
+import im.fir.sdk.FIR;
+import im.fir.sdk.VersionCheckCallback;
 
 
 public class SplashActivity extends BaseActivity {
@@ -120,76 +122,137 @@ public class SplashActivity extends BaseActivity {
     }
 
     public void checkVersion() {
-        new Thread(new Runnable() {
+        FIR.checkForUpdateInFIR("f3c345fdf2b447ff0d5294772b42eed2", new VersionCheckCallback() {
             @Override
-            public void run() {
+            public void onSuccess(String versionJson) {
                 Message msg = Message.obtain();
                 Bundle bundle = new Bundle();
-                String baseUrl = "http://fir.im/api/v2/app/version/%s?token=%s";
-                //下面是正式的 版本调整
-                // String checkUpdateUrl = String.format(baseUrl, "553ca95096a9fc5c14001802", "39d16f30ebf111e4a2da4efe6522248a4b9d9ed4");
-                String checkUpdateUrl = String.format(baseUrl, "56d8305ea1de523fd2000010", "653b6b146dd19b4ce30dea9bbfa4a1e2");
+                Log.i("fir", "check from fir.im success! " + "\n" + versionJson);
                 try {
-                    URL url = new URL(checkUpdateUrl);
-                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                    urlConnection.setRequestMethod("GET");// 设置请求的方式
-                    urlConnection.setReadTimeout(5000);// 设置超时的时间
-                    urlConnection.setConnectTimeout(5000);// 设置链接超时的时间
-                    if (urlConnection.getResponseCode() == urlConnection.HTTP_OK) {
-                        // 获取响应的输入流对象
-                        InputStream is = urlConnection.getInputStream();
-                        // 创建字节输出流对象
-                        ByteArrayOutputStream os = new ByteArrayOutputStream();
-                        // 定义读取的长度
-                        int len = 0;
-                        // 定义缓冲区
-                        byte buffer[] = new byte[1024];
-                        // 按照缓冲区的大小，循环读取
-                        while ((len = is.read(buffer)) != -1) {
-                            // 根据读取的长度写入到os对象中
-                            os.write(buffer, 0, len);
-                        }
-                        // 释放资源
-                        is.close();
-                        os.close();
-                        // 返回字符串
-                        String firResponse = new String(os.toByteArray());
-                        JSONObject versionJsonObj = new JSONObject(firResponse);
-                        //FIR上当前的versionCode
-                        int firVersionCode = Integer.parseInt(versionJsonObj.getString("version"));
-                        //FIR上当前的versionName
-                        String firVersionName = versionJsonObj.getString("versionShort");
-                        PackageManager pm = getApplicationContext().getPackageManager();
-                        PackageInfo pi = pm.getPackageInfo(getApplicationContext().getPackageName(),
-                                PackageManager.GET_ACTIVITIES);
-                        if (pi != null) {
-                            int currentVersionCode = pi.versionCode;
-                            String currentVersionName = pi.versionName;
+                    String firVersionCode_str = JSONUtils.ParseJSON(versionJson, "version");
+                    int firVersionCode = Integer.parseInt(firVersionCode_str);
+                    String firVersionName = JSONUtils.ParseJSON(versionJson, "versionShort");
+                    PackageManager pm = getApplicationContext().getPackageManager();
+                    PackageInfo pi = pm.getPackageInfo(getApplicationContext().getPackageName(),
+                            PackageManager.GET_ACTIVITIES);
+                    if(pi!=null){
+                        int currentVersionCode = pi.versionCode;
+                        String currentVersionName = pi.versionName;
 //                            Log.i("当前版本",currentVersionCode+"");
 //                            Log.i("查看版本",firVersionCode+"");
-                            if (firVersionCode > currentVersionCode) {
-                                //需要更新
+                        if (firVersionCode > currentVersionCode) {
+                            //需要更新
+                            bundle.putBoolean("isupdate", true);
+                        } else if (firVersionCode == currentVersionCode) {
+                            //如果本地app的versionCode与FIR上的app的versionCode一致，则需要判断versionName.
+                            if (!currentVersionName.equals(firVersionName)) {
                                 bundle.putBoolean("isupdate", true);
-                            } else if (firVersionCode == currentVersionCode) {
-                                //如果本地app的versionCode与FIR上的app的versionCode一致，则需要判断versionName.
-                                if (!currentVersionName.equals(firVersionName)) {
-                                    bundle.putBoolean("isupdate", true);
-                                }
-                            } else {
-                                //不需要更新,当前版本高于FIR上的app版本.
-                                bundle.putBoolean("isupdate", false);
                             }
-                            msg.what = UPDATE;
-                            msg.setData(bundle);
+                        } else {
+                            //不需要更新,当前版本高于FIR上的app版本.
+                            bundle.putBoolean("isupdate", false);
                         }
+                        msg.what = UPDATE;
+                        msg.setData(bundle);
                     }
+
+
                 } catch (Exception e) {
                     e.printStackTrace();
                     msg.what = UN_UPDATE;
                 }
                 MyHandler.sendMessage(msg);
             }
-        }).start();
+
+            @Override
+            public void onFail(Exception exception) {
+//                Log.i("fir", "check fir.im fail! " + "\n" + exception.getMessage());
+            }
+
+            @Override
+            public void onStart() {
+//                Toast.makeText(getApplicationContext(), "正在获取", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFinish() {
+//                Toast.makeText(getApplicationContext(), "获取完成", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                Message msg = Message.obtain();
+//                Bundle bundle = new Bundle();
+//                String baseUrl = "http://fir.im/api/v2/app/version/%s?token=%s";
+//                //下面是正式的 版本调整
+//                // String checkUpdateUrl = String.format(baseUrl, "553ca95096a9fc5c14001802", "39d16f30ebf111e4a2da4efe6522248a4b9d9ed4");
+//                String checkUpdateUrl = String.format(baseUrl, "56d8305ea1de523fd2000010", "653b6b146dd19b4ce30dea9bbfa4a1e2");
+//                try {
+//                    URL url = new URL(checkUpdateUrl);
+//                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+//                    urlConnection.setRequestMethod("GET");// 设置请求的方式
+//                    urlConnection.setReadTimeout(5000);// 设置超时的时间
+//                    urlConnection.setConnectTimeout(5000);// 设置链接超时的时间
+//                    int code = urlConnection.getResponseCode();
+//                    Log.d("test", "test");
+//                    if (urlConnection.getResponseCode() == urlConnection.HTTP_OK) {
+//                        // 获取响应的输入流对象
+//                        InputStream is = urlConnection.getInputStream();
+//                        // 创建字节输出流对象
+//                        ByteArrayOutputStream os = new ByteArrayOutputStream();
+//                        // 定义读取的长度
+//                        int len = 0;
+//                        // 定义缓冲区
+//                        byte buffer[] = new byte[1024];
+//                        // 按照缓冲区的大小，循环读取
+//                        while ((len = is.read(buffer)) != -1) {
+//                            // 根据读取的长度写入到os对象中
+//                            os.write(buffer, 0, len);
+//                        }
+//                        // 释放资源
+//                        is.close();
+//                        os.close();
+//                        // 返回字符串
+//                        String firResponse = new String(os.toByteArray());
+//                        JSONObject versionJsonObj = new JSONObject(firResponse);
+//                        //FIR上当前的versionCode
+//                        int firVersionCode = Integer.parseInt(versionJsonObj.getString("version"));
+//                        //FIR上当前的versionName
+//                        String firVersionName = versionJsonObj.getString("versionShort");
+//                        PackageManager pm = getApplicationContext().getPackageManager();
+//                        PackageInfo pi = pm.getPackageInfo(getApplicationContext().getPackageName(),
+//                                PackageManager.GET_ACTIVITIES);
+//                        if (pi != null) {
+//                            int currentVersionCode = pi.versionCode;
+//                            String currentVersionName = pi.versionName;
+////                            Log.i("当前版本",currentVersionCode+"");
+////                            Log.i("查看版本",firVersionCode+"");
+//                            if (firVersionCode > currentVersionCode) {
+//                                //需要更新
+//                                bundle.putBoolean("isupdate", true);
+//                            } else if (firVersionCode == currentVersionCode) {
+//                                //如果本地app的versionCode与FIR上的app的versionCode一致，则需要判断versionName.
+//                                if (!currentVersionName.equals(firVersionName)) {
+//                                    bundle.putBoolean("isupdate", true);
+//                                }
+//                            } else {
+//                                //不需要更新,当前版本高于FIR上的app版本.
+//                                bundle.putBoolean("isupdate", false);
+//                            }
+//                            msg.what = UPDATE;
+//                            msg.setData(bundle);
+//                        }
+//                    }
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                    msg.what = UN_UPDATE;
+//                }
+//                MyHandler.sendMessage(msg);
+//            }
+//        }).start();
     }
 
     /**
